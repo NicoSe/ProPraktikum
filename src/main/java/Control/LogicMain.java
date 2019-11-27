9b9eca8a16601bb9f918f8b870aff593934620b9
Adrian <git@adrianmxb.com>
2019-11-27 15:19:49 +0100

    add a test main function to shoot form console needs placement function

--- src/main/java/Control/LogicMain.java
+++ src/main/java/Control/LogicMain.java
@@ -20,9 +20,23 @@ public class LogicMain {
         Grid g = new Grid(Integer.parseInt(args[0]));
         Scanner s = new Scanner(System.in);
 
+        System.out.println("enter command.");
+        System.out.println("e.g. shoot x y");
         while(s.hasNextLine()) {
+            try {
+                String[] split = s.nextLine().split(" ");
 
+                //simulate shit with x: split[1]&y: split[2]
+                if (split[0].equals("shoot")) {
+                    int x = Integer.parseInt(split[1]);
+                    int y = Integer.parseInt(split[2]);
+
+                    System.out.format("boom at x:%d y:%d (local: %d) returns %s \n", x, y, g.convertXYToLocal(x, y), g.shoot(g.convertXYToLocal(x, y)));
+                }
+            } catch(Exception e) {
+                System.out.println("error! enter command.");
+                System.out.println("e.g. shoot x y");
+            }
         }
-
     }
 }
