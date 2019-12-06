13ccdd2bb582852eedf3c3033f7be607015ce19d
Lukas <lukasvogrinc@hotmail.de>
2019-12-06 23:18:23 +0100

    Schiffe ins Grid zu legen sollte jetzt funktionieren

--- src/main/java/Control/LogicMain.java
+++ src/main/java/Control/LogicMain.java
@@ -1,14 +1,22 @@
 package Control;
 
+import Logic.Character;
 import Logic.Grid2D;
 import Logic.Ship;
 import Logic.ShipHarbor;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class LogicMain {
     //args[0]: size;
     public static void main(String[] args) {
+
+        ShipHarbor harbor = new ShipHarbor();
+        harbor.load();
+
         if(args.length <= 0) {
             System.exit(1);
         }
@@ -17,6 +25,199 @@ public class LogicMain {
         g.generateRandom();
         System.out.printf("Grid:\n%s\n", g);
         Scanner s = new Scanner(System.in);
+
+        int border = harbor.getBorder(g.getBound());
+        int counter = harbor.getTotalShipCount(g.getBound());
+
+        ArrayList<Ship> shipArrayList = new ArrayList<>();
+
+        while(counter > 0)
+        {
+            System.out.println("Available ships:");
+
+            for(int i = 0; i < border; i++)
+            {
+                System.out.println(harbor.getCopyOfHarborData(g.getBound()).get(i)+"er: " + harbor.getCopyOfHarborData(g.getBound()).get(i));
+            }
+            System.out.println("Please choose a ship and the Position");
+            System.out.println("e.g. 3er Position x y");
+
+            String userIO = s.nextLine();
+            while(s.hasNextLine())
+            {
+                try
+                {
+                    String[] split = s.nextLine().split(" ");
+
+                    if(split[1].equals("Position"))
+                    {
+                        int x = Integer.parseInt(split[2]);
+                        int y = Integer.parseInt(split[3]);
+
+                        if(border == 2)
+                        {
+                            switch (userIO)
+                            {
+                                case ("3er"):
+                                    //i = total count of all 3er ships
+                                    int i = harbor.getAmounts(g.getBound()).get(0);
+                                    --i;
+                                    if(i > 0)
+                                    {//decrease total amount of 3er ships
+                                        harbor.getAmounts(g.getBound()).set(0,i);
+                                        shipArrayList.add(new Ship(harbor.getSize(g.getBound()).get(0)));//create new ship object with the wished size
+                                        g.put(x,y,shipArrayList.get(0));//put the ship on the grid
+                                    }else
+                                        {//the wanted ship isn't available
+                                            System.out.println("Please choose a different ship");
+                                            counter++;
+                                        }
+                                    break;
+                                case ("2er"):
+                                    i = harbor.getAmounts(g.getBound()).get(1);
+                                    --i;
+                                    if(i > 0)
+                                    {
+                                        harbor.getAmounts(g.getBound()).set(1,i);
+                                        shipArrayList.add(new Ship(harbor.getSize(g.getBound()).get(1)));
+                                        g.put(x,y,shipArrayList.get(1));
+                                    }else
+                                    {
+                                        System.out.println("Please choose a different ship");
+                                        counter++;
+                                    }
+                                    break;
+                                default:
+                                    System.out.println("Invalid input");
+                                    counter++;
+                            }
+                            counter--;
+                        }else if(border == 3)
+                        {
+                            switch (userIO)
+                            {
+                                case ("4er"):
+                                    int i = harbor.getAmounts(g.getBound()).get(0);
+                                    --i;
+                                    if(i > 0)
+                                    {
+                                        harbor.getAmounts(g.getBound()).set(0,i);
+                                        shipArrayList.add(new Ship(harbor.getSize(g.getBound()).get(0)));
+                                        g.put(x,y,shipArrayList.get(0));
+                                    }else
+                                    {
+                                        System.out.println("Please choose a different ship");
+                                        counter++;
+                                    }
+                                    break;
+                                case ("3er"):
+                                    i = harbor.getAmounts(g.getBound()).get(1);
+                                    --i;
+                                    if(i > 0)
+                                    {
+                                        harbor.getAmounts(g.getBound()).set(1,i);
+                                        shipArrayList.add(new Ship(harbor.getSize(g.getBound()).get(1)));
+                                        g.put(x,y,shipArrayList.get(1));
+                                    }else
+                                    {
+                                        System.out.println("Please choose a different ship");
+                                        counter++;
+                                    }
+                                    break;
+                                case ("2er"):
+                                    i = harbor.getAmounts(g.getBound()).get(2);
+                                    --i;
+                                    if(i > 0)
+                                    {
+                                        harbor.getAmounts(g.getBound()).set(2,i);
+                                        shipArrayList.add(new Ship(harbor.getSize(g.getBound()).get(2)));
+                                        g.put(x,y,shipArrayList.get(2));
+                                    }else
+                                    {
+                                        System.out.println("Please choose a different ship");
+                                        counter++;
+                                    }
+                                    break;
+                                default:
+                                    System.out.println("Invalid input");
+                                    counter++;
+                            }
+                            counter--;
+                        }else
+                        {
+                            switch (userIO)
+                            {
+                                case ("5er"):
+                                    int i = harbor.getAmounts(g.getBound()).get(0);
+                                    --i;
+                                    if(i > 0)
+                                    {
+                                        harbor.getAmounts(g.getBound()).set(0,i);
+                                        shipArrayList.add(new Ship(harbor.getSize(g.getBound()).get(0)));
+                                        g.put(x,y,shipArrayList.get(0));
+                                    }else
+                                    {
+                                        System.out.println("Please choose a different ship");
+                                        counter++;
+                                    }
+                                    break;
+                                case ("4er"):
+                                    i = harbor.getAmounts(g.getBound()).get(1);
+                                    --i;
+                                    if(i > 0)
+                                    {
+                                        harbor.getAmounts(g.getBound()).set(1,i);
+                                        shipArrayList.add(new Ship(harbor.getSize(g.getBound()).get(1)));
+                                        g.put(x,y,shipArrayList.get(1));
+                                    }else
+                                    {
+                                        System.out.println("Please choose a different ship");
+                                        counter++;
+                                    }
+                                    break;
+                                case ("3er"):
+                                    i = harbor.getAmounts(g.getBound()).get(2);
+                                    --i;
+                                    if(i > 0)
+                                    {
+                                        harbor.getAmounts(g.getBound()).set(2,i);
+                                        shipArrayList.add(new Ship(harbor.getSize(g.getBound()).get(2)));
+                                        g.put(x,y,shipArrayList.get(2));
+                                    }else
+                                    {
+                                        System.out.println("Please choose a different ship");
+                                        counter++;
+                                    }
+                                    break;
+                                case ("2er"):
+                                    i = harbor.getAmounts(g.getBound()).get(3);
+                                    --i;
+                                    if(i > 0)
+                                    {
+                                        harbor.getAmounts(g.getBound()).set(3,i);
+                                        shipArrayList.add(new Ship(harbor.getSize(g.getBound()).get(3)));
+                                        g.put(x,y,shipArrayList.get(3));
+                                    }else
+                                    {
+                                        System.out.println("Please choose a different ship");
+                                        counter++;
+                                    }
+                                    break;
+                                default:
+                                    System.out.println("Invalid input");
+                                    counter++;
+                            }
+                            counter--;
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    System.out.println("Invalid command! reenter command");
+                    System.out.println("e.g. 3er Position x y");
+                }
+            }
+        }
 
         System.out.println("enter command.");
         System.out.println("e.g. shoot x y");
--- src/main/java/Logic/ShipHarbor.java
+++ src/main/java/Logic/ShipHarbor.java
@@ -3,6 +3,7 @@ package Logic;
 import java.io.*;
 import java.lang.ClassLoader;
 import java.nio.charset.StandardCharsets;
+import java.sql.SQLOutput;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -80,75 +81,45 @@ public class ShipHarbor {
                 harbor.put(gridBounds, currentBoundShipData);
                 System.out.println(line);
             }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+    public List<Integer> getSize(int bound)
+    {//the value at index 0 is dependent of the grid
+        List<Integer> sizes = new ArrayList<>();
 
-    public List<Integer> getValues(int bound)//Use if the souts need to be in the main method
-    {
-       List<Integer> values = new ArrayList<>(harbor.get(bound));
-       return values;
-    }
-
-    public void put(int bound)
-    {
-        load();
-        List<Integer> values = new ArrayList<>(harbor.get(bound));
-
-        while(values.get(0) != 0 || values.get(1) != 0 || values.get(2) != 0 || values.get(3) != 0)
+        for(int i = 0; i < getBorder(bound); i++)
         {
-            System.out.println("Available ships:");
-            if(values.get(0) > 0)
-            {
-                System.out.println("5er: " + values.get(0));
-            }
-            if(values.get(1) > 0)
-            {
-                System.out.println("4er: " + values.get(1));
-            }
-            if(values.get(2) > 0)
-            {
-                System.out.println("3er: " + values.get(2));
-            }
-            if(values.get(3) > 0)
-            {
-                System.out.println("2er: " + values.get(3));
-            }
-
-            System.out.println("Please choose a ship");
-
-            Scanner s = new Scanner(System.in);
-            String userIO = s.nextLine();
-
-            switch (userIO)
-            {                                       //decreases the amount of ships by 1
-                case"5er":
-                    int i = values.get(0);
-                    --i;
-                    values.set(0,i);
-                    break;
-                case"4er":
-                    i = values.get(1);
-                    --i;
-                    values.set(1,i);
-                    break;
-                case"3er":
-                    i = values.get(2);
-                    --i;
-                    values.set(2,i);
-                    break;
-                case"2er":
-                    i = values.get(3);
-                    --i;
-                    values.set(3,i);
-                    break;
-                default:
-                    System.out.println("Invalid input");
-            }                                       //if the stupid user chooses a not available ship nothing happens. Good or Bad?
-
-            System.out.println("Choose the Position ,x y, for your ship:");
-
+//if the border > 9, the size at index 0 will be 5, if border < 9, the size at index 0 will be 4...
+            sizes.add(i, getCopyOfHarborData(bound).get(i).size);
         }
+        return sizes;
+    }
+    public List<Integer> getAmounts(int bound)
+    {
+        List<Integer> amounts = new ArrayList<>();
+
+        for(int i = 0; i < getBorder(bound); i++)
+        {
+            amounts.add(i, getCopyOfHarborData(bound).get(i).amount);
+        }
+        return amounts;
+    }
+    public int getBorder(int bound)
+    {//the size of getCopyOfHarborData is dependent of the grid
+        int border = 0;
+        if(bound == 5)
+        {
+            border = 2;
+        }else if(bound < 9)
+        {
+            border = 3;
+        }else
+        {
+            border = 4;
+        }
+        return border;
     }
 }
