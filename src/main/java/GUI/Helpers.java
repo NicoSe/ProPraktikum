1da91498947402ed54643806f6a0b73b82607a7c
Adrian <git@adrianmxb.com>
2020-01-20 22:54:23 +0100

    try to fix crashes

--- src/main/java/GUI/Helpers.java
+++ src/main/java/GUI/Helpers.java
@@ -17,30 +17,32 @@ public class Helpers {
     *musictype can be 0(music) and 1(SFX)
     */
     public static void playSFX(String filepath, int musicType){
-        try{
-            Clip clip = AudioSystem.getClip();
-            AudioInputStream inputStream = AudioSystem.getAudioInputStream(MainFrame.class.getResourceAsStream(filepath));
-            clip.open(inputStream);
+        new Thread(() -> {
+            try{
+                Clip clip = AudioSystem.getClip();
+                AudioInputStream inputStream = AudioSystem.getAudioInputStream(MainFrame.class.getResourceAsStream(filepath));
+                clip.open(inputStream);
 
-            FloatControl volControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
-            if(musicType == 0){
-                float vol = (float) OptionsHandler.getMusicVolume()/100;
-                vol = 80 * vol;
-                vol = -80 + vol;
-                volControl.setValue((float) vol);
-            }
-            else if(musicType == 1){
+                FloatControl volControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+                if(musicType == 0){
+                    float vol = (float) OptionsHandler.getMusicVolume()/100;
+                    vol = 80 * vol;
+                    vol = -80 + vol;
+                    volControl.setValue((float) vol);
+                }
+                else if(musicType == 1){
 
-                float vol = (float) OptionsHandler.getSFXVolume()/100;
-                vol = 80 * vol;
-                vol = -80 + vol;
-                volControl.setValue(vol);
+                    float vol = (float) OptionsHandler.getSFXVolume()/100;
+                    vol = 80 * vol;
+                    vol = -80 + vol;
+                    volControl.setValue(vol);
+                }
+                else return;
+                clip.start();
+            }catch(Exception e){
+                System.err.println(e.getMessage());
             }
-            else return;
-            clip.start();
-        }catch(Exception e){
-            System.err.println(e.getMessage());
-        }
+        }).start();
     }
 
 
