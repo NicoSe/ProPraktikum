cfd2762b20a9c3b19f0c0e5171b7359efd302d77
Dimsch <57183037+Diimsch@users.noreply.github.com>
2020-01-20 11:34:42 +0100

    addi Galaxy brain

--- src/main/java/Network/Server2.java
+++ src/main/java/Network/Server2.java
@@ -193,10 +193,17 @@ public class Server2 implements Connector{
     public boolean Close(){
         try {
             Close_Socket = true;
-            dis.close();
-            dos.close();
+            if(dis != null) {
+                dis.close();
+            }
+            if(dos != null) {
+                dos.close();
+            }
+
             Server_Socket.close();
-            Client_Socket.close();
+            if(Client_Socket != null) {
+                Client_Socket.close();
+            }
         } catch (IOException e) {
             System.out.println("<S>Sockets couldnßt be closed!");
             return false;
