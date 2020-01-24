f36b80cc9c7b5d13cf3489402ecbc025ab2c62c0
Thomas <Th.schmidt97@outlook.de>
2020-01-25 00:40:23 +0100

    you loose ;-; You win genosse

--- src/main/java/GUI/MainFrame.java
+++ src/main/java/GUI/MainFrame.java
@@ -21,7 +21,7 @@ import Misc.GridState;
 import Network.*;
 
 public class MainFrame {
-
+    private JLabel lblComrade;
     ///net
     Connector net;
     Connector foe;
@@ -312,6 +312,10 @@ public class MainFrame {
         lblTitle.setIcon(new ImageIcon(getClass().getResource("/Sprites/Title_v11.gif")));
         lblTitle.setAlignmentX(Component.CENTER_ALIGNMENT);
         pnlButton.add(lblTitle);
+
+        //soviet
+        lblComrade = new JLabel();
+        lblComrade.setIcon(new ImageIcon(getClass().getResource("/Sprites/sovietflag.gif")));
 
         //Loading animation
         lblLoading =  new JLabel();
@@ -1983,6 +1987,9 @@ public class MainFrame {
                     if(answer == 2 && --foeAliveCount <= 0) {
                         SwingUtilities.invokeLater(() -> {
                             Helpers.playSFX("/SFX/youwincomrad.wav", 1);
+                            pnlFoeGrid.setVisible(false);
+                            pnlFoeGrid.add(lblComrade);
+                            pnlFoeGrid.setVisible(true);
                             JOptionPane.showMessageDialog(null, "meh, you won... Ok, exits the game.");
                             net.Close();
                             System.exit(0);
