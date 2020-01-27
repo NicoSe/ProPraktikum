a9f4e680f8d0bcb158af809c22814ca9de582257
ShunYagami <nico24sg@gmail.com>
2020-01-27 16:45:54 +0100

    fix bug with fullscreen

--- src/main/java/GUI/MainFrame.java
+++ src/main/java/GUI/MainFrame.java
@@ -1382,7 +1382,6 @@ public class MainFrame {
         else{
             lblFullscreenPicture.setIcon(new ImageIcon(ImageIO.read(getClass().getResource("/Sprites/Checkbox_clear.png")).getScaledInstance(40,40,Image.SCALE_SMOOTH)));
         }
-        lblFullscreenPicture.setIcon(new ImageIcon(ImageIO.read(getClass().getResource("/Sprites/Checkbox_clear.png")).getScaledInstance(40,40,Image.SCALE_SMOOTH)));
         lblFullscreenPicture.setAlignmentX(Component.CENTER_ALIGNMENT);
         lblFullscreenPicture.addMouseListener(new MouseAdapter(){
             @Override
